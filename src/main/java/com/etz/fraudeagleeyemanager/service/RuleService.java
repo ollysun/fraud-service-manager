0fa5ab1c6a42c43797d2bdb68a39d861c9268889
moses olalere <olalere.moses@etranzactng.com>
2021-11-19 09:24:26 +0100

    fix bug on servicerule

--- src/main/java/com/etz/fraudeagleeyemanager/service/RuleService.java
+++ src/main/java/com/etz/fraudeagleeyemanager/service/RuleService.java
@@ -264,12 +264,12 @@ public class RuleService {
 
 		// verify the dataset on rule
 		List<ServiceDataSet> serviceDataSetList = productDataSetRepository.findByServiceId(request.getServiceId());
-		if (serviceDataSetList.isEmpty()){
-			throw new ResourceNotFoundException("Service Dataset  not found for serviceId " + request.getServiceId());
-		}
-		for (ServiceDataSet sd: serviceDataSetList){
-			datasetList.add(sd.getFieldName());
-		}
+//		if (serviceDataSetList.isEmpty()){
+//			throw new ResourceNotFoundException("Service Dataset  not found for serviceId " + request.getServiceId());
+//		}
+//		for (ServiceDataSet sd: serviceDataSetList){
+//			datasetList.add(sd.getFieldName());
+//		}
 
 
 
@@ -285,9 +285,9 @@ public class RuleService {
 //		}
 
 		
-		if (!Objects.isNull(request.getNotificationGroupId()) && !notificationGroupRepository.findById(request.getNotificationGroupId()).isPresent()) {
-			throw new ResourceNotFoundException("Notification Group Id Not found for Id " + request.getNotificationGroupId());
-		}
+//		if (!Objects.isNull(request.getNotificationGroupId()) && !notificationGroupRepository.findById(request.getNotificationGroupId()).isPresent()) {
+//			throw new ResourceNotFoundException("Notification Group Id Not found for Id " + request.getNotificationGroupId());
+//		}
 
 		ServiceRule serviceRuleEntity = new ServiceRule();
 		List<ServiceRule> createdServiceRuleEntity = new ArrayList<>();
@@ -424,7 +424,7 @@ public class RuleService {
 		}
 		addAccountProductEntityToRedis(persistedServiceRule);
 		// create & user notification
-		appUtil.createUserNotification(AppConstant.SERVICE_RULE, persistedServiceRule.getServiceId() + "_" + persistedServiceRule.getRule().toString(), createdBy);
+		appUtil.createUserNotification(AppConstant.SERVICE_RULE, persistedServiceRule.getServiceId() + "_" + persistedServiceRule.getRuleId(), createdBy);
 		return persistedServiceRule;
 	}
 	
